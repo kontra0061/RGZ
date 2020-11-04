600ce33781a4d20ce75e35f38a81e3948436915e
kontra0061 <60006937+kontra0061@users.noreply.github.com>
2020-11-05 00:14:48 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+let upButton = document.querySelector('.up-button');
+
+window.onscroll = function () {
+  if (window.pageYOffset > 200) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+  }
+
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
+
+
+
+
+
 new Swiper('.swiper-container', {
     sliderPerView: 1,
     loop: true,
